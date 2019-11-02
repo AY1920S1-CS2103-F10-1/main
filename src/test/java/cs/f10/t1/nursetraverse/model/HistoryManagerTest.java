3da7d80c965c0a6c43a784f0389b7acac403a890
sandydays <sandhyar725@gmail.com>
2019-11-02 13:40:11 +0800

    Fix more checkstyle issues

--- src/test/java/cs/f10/t1/nursetraverse/model/HistoryManagerTest.java
+++ src/test/java/cs/f10/t1/nursetraverse/model/HistoryManagerTest.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
-import cs.f10.t1.nursetraverse.model.appointment.Appointment;
 import cs.f10.t1.nursetraverse.testutil.DummyMutatorCommand;
 import javafx.collections.ObservableList;
 
